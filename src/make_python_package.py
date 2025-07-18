7ab609c0750cbbbb1c2b8e0a48e5447d673ae75f
Philipp Meder <148459685+PhilippMeder@users.noreply.github.com>
2025-07-18 12:39:35 +0200

    Added options for author, mail, repository, ... to package creator

--- src/make_python_package.py
+++ src/make_python_package.py
@@ -1,5 +1,6 @@
 import argparse
 import requests
+from dataclasses import dataclass, fields
 from pathlib import Path
 
 
@@ -21,17 +22,127 @@ class Structure:
         }
 
 
+@dataclass(kw_only=True)
+class ProjectSettings:
+
+    name: str = "PACKAGENAME"
+    description: str = "PACKAGEDESCRIPTION"
+    license_id: str = None
+    author_name: str = "AUTHORNAME"
+    author_mail: str = "AUTHORMAIL@SOMETHING.com"
+    github_username: str = "USERNAME"
+    github_repositoryname: str = "REPOSITORYNAME"
+    changelog: str = None
+    documentation: str = None
+    download: str = None
+    funding: str = None
+    homepage: str = None
+    issues: str = None
+    releasenotes: str = None
+    source: str = None
+
+    def __getattribute__(self, name):
+        """Return value of attribute 'name', but take care of default values."""
+        value = super().__getattribute__(name)  # getattr(self, name)
+        if value is None:
+            match name:
+                case "changelog" | "releasenotes":
+                    return f"{self.github}/commits"
+                case "documentation":
+                    return f"{self.github}/README.md"
+                case "download" | "homepage":
+                    return self.github
+                case "issues":
+                    return f"{self.github}/issues"
+                case "source":
+                    return f"{self.github}.git"
+                case "funding" | _:
+                    return value
+        else:
+            return value
+
+    @property
+    def github(self) -> str:
+        """Return the link to the github repository."""
+        return f"https://github.com/{self.github_username}/{self.github_repositoryname}"
+
+    @property
+    def github_owner(self) -> str:
+        """Return the link to the owner of the github repository."""
+        return f"https://github.com/{self.github_username}"
+
+    @staticmethod
+    def get_url_fields():
+        """Return a tuple of field names that represent a project url."""
+        return (
+            "changelog",
+            "documentation",
+            "download",
+            "funding",
+            "homepage",
+            "issues",
+            "releasenotes",
+            "source",
+        )
+
+    @property
+    def urls(self) -> dict[str]:
+        """Return '{name: url}' dictionary for project urls."""
+        names = self.get_url_fields()
+        return {
+            name: _url for name in names if (_url := getattr(self, name)) is not None
+        }
+
+    @classmethod
+    def add_to_argparser(
+        cls, parser: argparse.ArgumentParser, ignore: tuple[str] | list[str] = None
+    ):
+        """Add all fields that are not in 'ignore' to an argument parser."""
+        if ignore is None:
+            ignore = []
+        for field in fields(cls):
+            if field.name in ignore:
+                continue
+            argument = f"--{field.name.replace("_", "-")}"
+            help_str = f"{field.name.replace("_", " ")}"
+            if field.name in cls.get_url_fields():
+                help_str = f"url to {help_str}  (default: composed from other settings)"
+            else:
+                help_str = f"{help_str} (default: {field.default})"
+            options = {
+                "type": field.type,
+                "help": help_str,
+                "default": field.default,
+            }
+            parser.add_argument(argument, **options)
+
+    @classmethod
+    def from_argparser(cls, args: argparse.Namespace):
+        """Return instance of this class with options set to the values of 'args'."""
+        args_dict = vars(args)
+        names = [field.name for field in fields(cls)]
+        options = {name: value for name, value in args_dict.items() if name in names}
+
+        return cls(**options)
+
+
 class FileContent(dict):
 
-    def __init__(self, license: str = None, **kwargs):
+    def __init__(self, project_settings: ProjectSettings, **kwargs):
+        self.project_settings = project_settings
         kwargs.setdefault(".gitignore", self.get_gitignore())
-        kwargs.setdefault("LICENSE", self.get_license(license))
+        kwargs.setdefault("LICENSE", self.get_license())
+        try:
+            self.license_name = kwargs["LICENSE"].splitlines()[0]
+        except Exception as err:
+            self.license_name = "LICENSENAME"
         kwargs.setdefault("pyproject.toml", self.get_pyproject_toml())
         kwargs.setdefault("README.md", self.get_readme())
         super().__init__(**kwargs)
 
     @staticmethod
     def get_gitignore():
+        """Return default content for '.gitignore'."""
         return (
             "__pycache__\n"
             "#.gitignore\n"
@@ -43,42 +154,46 @@ class FileContent(dict):
             "*.egg-info"
         )
 
-    @staticmethod
-    def get_license(license: str = None):
-        if license is None:
+    def get_license(self):
+        """Return license text according to 'project_settings.license_id'."""
+        if self.project_settings.license_id is None:
             return ""
         try:
-            return get_license(license)
+            return get_license(self.project_settings.license_id)
         except Exception as err:
             return ""
 
-    @staticmethod
-    def get_pyproject_toml():
-        github_repo = "https://github.com/USERNAME/PACKAGENAME"
+    def get_pyproject_toml(self):
+        """Return default value for 'pyproject.toml' according to 'project_settings'."""
+        project = self.project_settings
+        author_str = f"""name="{project.author_name}", email="{project.author_mail}" """
+        url_str = "".join(
+            [
+                f"""{name.capitalize()} = "{_url}"\n"""
+                for name, _url in project.urls.items()
+            ]
+        )
         return (
-            """[project]\nname = "PACKAGENAME"\nversion = "0.1"\n"""
-            """authors = [{ name="AUTHORNAME", email="AUTHORMAIL@SOMETHING.com" },]\n"""
-            """description = "PACKAGEDESCRIPTION"\nreadme = "README.md"\n"""
+            f"""[project]\nname = "{project.name}"\nversion = "0.1"\n"""
+            f"""authors = [{{ {author_str} }},]\n"""
+            f"""description = "{project.description}"\nreadme = "README.md"\n"""
             """license = { file = "LICENSE" }\nrequires-python = ">=3.XX"\n"""
             """dependencies = []\n"""
             """classifiers=[\n    "Programming Language :: Python :: 3",\n    """
             """"Operating System :: OS Independent",\n]"""
-            """\n\n[project.urls]\n"""
-            f"""Homepage = "{github_repo}"\n"""
-            f"""Documentation = "{github_repo}"\n"""
-            f"""Repository = "{github_repo}.git"\n"""
-            f"""Issues = "{github_repo}/issues"\n"""
-            f"""Changelog = "{github_repo}/commits"\n"""
+            f"""\n\n[project.urls]\n{url_str}"""
         )
 
-    @staticmethod
-    def get_readme():
+    def get_readme(self):
+        """Return default value for 'README' according to 'project_settings'."""
+        project = self.project_settings
         return (
-            "# PACKAGENAME\n\n**PACKAGEDESCRIPTION**\n\n"
-            "Developed and maintained by [AUTHORNAME](https://github.com/USERNAME).\n\n"
-            "* **Source code:** https://github.com/USERNAME/PACKAGENAME\n"
-            "* **Report bugs:** https://github.com/USERNAME/PACKAGENAME/issues\n\n"
-            "## License\n\nDistributed under the [LICENSENAME](./LICENSE).\n\n"
+            f"# {project.name}\n\n**{project.description}**\n\n"
+            "Developed and maintained by "
+            f"[{project.author_name}]({project.github_owner}).\n\n"
+            f"* **Source code:** {project.source}\n"
+            f"* **Report bugs:** {project.issues}\n\n"
+            f"## License\n\nDistributed under the [{self.license_name}](./LICENSE).\n\n"
             "## Features\n\n1. [FEATURE 1](#feature-1)\n2. [FEATURE 2](#feature-2)\n\n"
             "### FEATURE 1\n\n### FEATURE 2\n\n"
             "## Requirements"
@@ -104,18 +219,23 @@ def create_components(path: Path, structure: dict, file_content: dict = None):
 
 
 def create_package_structure(
-    destination_path: str | Path, name: str, ask: bool = True, license: str = None
+    destination_path: str | Path,
+    name: str,
+    ask: bool = True,
+    project_settings: ProjectSettings = None,
 ):
     """Get the wanted package structure and create it."""
+    if project_settings is None:
+        project_settings = ProjectSettings()
     parent_dir = Path(destination_path)
     structure = Structure.package(name)
-    file_content = FileContent(license=license)
+    file_content = FileContent(project_settings)
     if ask:
-        user_input = input(f"Create package '{name}' at '{parent_dir}'? (Y/n): ")
-        if user_input == "Y":
+        _input = input(f"Create package '{name}' at '{parent_dir.absolute()}'? (Y/n): ")
+        if _input == "Y":
             create_components(parent_dir, structure, file_content=file_content)
         else:
-            print(f"Creation aborted by user input '{user_input}'...")
+            print(f"Creation aborted by user input '{_input}'...")
     else:
         create_components(parent_dir, structure, file_content=file_content)
 
@@ -170,6 +290,12 @@ def get_sys_args():
         help="destination directory for the package structure",
     )
     parser.add_argument(
+        "-r",
+        "--autoname-repository",
+        action="store_true",
+        help="ignore the value of 'github_repositoryname' and set it to 'name'",
+    )
+    parser.add_argument(
         "-l",
         "--license",
         default=None,
@@ -180,6 +306,7 @@ def get_sys_args():
         action="store_true",
         help="list all available licenses and exit",
     )
+    ProjectSettings.add_to_argparser(parser, ignore=("license_id", "name"))
 
     return parser.parse_args()
 
@@ -190,6 +317,10 @@ if __name__ == "__main__":
         licenses_str = ", ".join(get_available_licenses().keys())
         print(f"Available licenses are:\n{licenses_str}")
     else:
+        project_settings = ProjectSettings.from_argparser(args)
+        project_settings.license_id = args.license
+        if args.autoname_repository:
+            project_settings.github_repositoryname = args.name
         create_package_structure(
-            args.destination, args.name, ask=True, license=args.license
+            args.destination, args.name, ask=True, project_settings=project_settings
         )
