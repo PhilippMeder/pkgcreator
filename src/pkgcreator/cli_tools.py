df70b37ace2e596331a853c3b2e86fbb315c5716
Philipp Meder <148459685+PhilippMeder@users.noreply.github.com>
2025-07-24 11:46:33 +0200

    Updated ConsitentFormatter to properly format commands for subparsers

--- src/pkgcreator/cli_tools.py
+++ src/pkgcreator/cli_tools.py
@@ -18,6 +18,11 @@ class ConsistentFormatter(argparse.HelpFormatter):
 
         Contrary to the usual argparse formatters, present choices will always be shown!
         """
+        # Handle subparser and subcommands (do not change their appearence)
+        if isinstance(action, argparse._SubParsersAction) or isinstance(
+            action, argparse._SubParsersAction._ChoicesPseudoAction
+        ):
+            return lambda tuple_size: (action.metavar or default_metavar,) * tuple_size
         # Different than usual argparse formatters: choices are always next to metavar
         if action.metavar is not None:
             result = action.metavar
